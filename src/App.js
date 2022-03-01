f3a1d6db8f8375ebcc134cdafd1030e349a38aed
nbur4556 <nbur4556@gmail.com>
2022-03-01 13:25:17 -0600

    fix: remove xr button

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import { useState } from 'react';
 
 // Components
 import SceneContainer from './components/SceneContainer';
-import XRButton from './components/XRButton';
 import LocationButton from './components/LocationButton';
 import PositionDisplay from './components/PositionDisplay';
 
 const App = () => {
-  const [showScene, setShowScene] = useState(false);
   const [currentPosition, setCurrentPosition] = useState({});
   const [carPosition, setCarPosition] = useState({});
 
   return <main>
-    <XRButton showScene={showScene} setShowScene={setShowScene} />
     <LocationButton setPosition={setCurrentPosition}>Current Position</LocationButton>
     <LocationButton setPosition={setCarPosition}>Car Position</LocationButton>
 
@@ -21,7 +18,7 @@ const App = () => {
     <h3>Car Position</h3>
     <PositionDisplay position={carPosition} />
 
-    {(showScene) ? <SceneContainer /> : null}
+    <SceneContainer />
   </main>
 }
 
